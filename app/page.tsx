ce85e6bcc51845888da2d59776c937e59e1be57e
YUGE0 <yugprajapati32@gmail.com>
2024-10-01 18:54:23 +0530

    Responsive Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -8,22 +8,22 @@ const index = 1
 export default function Home() {
   return (
     <div className="">
-      <div className="p-10">
+      <div className="sm:p-10">
       <ParallaxText baseVelocity={-3}>DESIGN</ParallaxText>
       <ParallaxText baseVelocity={3}>Develope</ParallaxText>
       </div>
       <div className="flex flex-wrap place-items-end">
         <Image height={0} width={800} src="/Profile.png" alt="image"/>
-        <div className="px-20">
+        <div className="sm:px-20">
           <h1 className="text-9xl font-work">Hello</h1>
           <h1 className="text-9xl font-work uppercase">It&#39;s me Yug</h1>
         </div>
       </div>
-      <div className="p-20">
+      <div className="sm:p-20">
         <h1 className="text-5xl font-work">My Work</h1>
         {projects.map((project) => (<WorkCom key={index} project={project} />))}
       </div>
-      <div className="p-20">
+      <div className="sm:p-20">
         <h1 className="text-5xl font-work">My Skills</h1>
       </div>
     </div>
